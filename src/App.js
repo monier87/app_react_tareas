ec4fb018f4414294795717c1ba40ff6d1084b05d
Carlos David <carlos.monier87@gmail.com>
2023-03-26 22:46:28 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png'
+import Tarea from './componentes/tarea';
+
+
+function App() {
+
+
+  return (
+    <div className='aplicacion-tareas'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img 
+          src={freeCodeCampLogo}
+          className='freecodecamp-logo' />
+      </div>
+      <div className='tareas-lista-principal'>
+        <h1> Mis Tareas</h1>
+        <Tarea  texto='Aprender React'/>  
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
